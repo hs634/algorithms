53ef41423d714cea7bd995455b8911a298b14720
Harsh Shah <hs634@cornell.edu>
2016-07-27 16:03:52 -0700

    Update morris_traversal.py

--- python/trees/morris_traversal.py
+++ python/trees/morris_traversal.py
@@ -1,9 +1,13 @@
+# 
 # Definition for a  binary tree node
 # class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
+
+# http://www.geeksforgeeks.org/inorder-tree-traversal-without-recursion-and-without-stack/
+#
 
 class BSTIterator(object):
     def __init__(self, root):
