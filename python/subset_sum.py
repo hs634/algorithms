f6f523dcb23efaebcc4be1e6afaf5e7723df8117
Harsh Shah <hs634@cornell.edu>
2016-07-29 08:14:51 -0700

    Update subset_sum.py

--- python/subset_sum.py
+++ python/subset_sum.py
@@ -1,1 +1,15 @@
 # http://www.geeksforgeeks.org/dynamic-programming-subset-sum-problem/
+"""
+public static int count(List<Integer> list, int n, int i) {
+    //System.out.print(list.size() + ", " + n);
+    //System.out.println();
+    if (n < 0 || i <= 0)
+      return 0;
+
+    int e = list.get(i);  // e is the i-th element in the list
+    if (e == n)
+      return 1 + count(list, n, i-1);   // Return 1 + check for more possibilities without picking e
+
+    return count(list, n, i-1) + count(list, n - e, i-1);   // Result if e is not picked + result if e is picked
+}
+"""
